e49aab0448329442d97143213389737e40c7439d
Jessica Mauvais <mauvais2@llnl.gov>
2022-11-15 08:44:09 -0800

    updated tests

--- atomsci/ddm/test/integrative/dc_models/test_dc_models.py
+++ atomsci/ddm/test/integrative/dc_models/test_dc_models.py
@@ -6,9 +6,9 @@ from atomsci.ddm.utils import llnl_utils
 # Train and Predict
 # -----
 def test_reg_config_H1_fit_AttentiveFPModel():
-    if not llnl_utils.is_lc_system():
-        assert True
-        return
+#    if not llnl_utils.is_lc_system():
+#        assert True
+#        return
         
     H1_init()
     train_and_predict('reg_config_H1_fit_AttentiveFPModel.json', prefix='H1') # crashes during run
