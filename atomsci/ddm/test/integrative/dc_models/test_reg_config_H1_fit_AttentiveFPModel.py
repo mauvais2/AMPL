d996d757c12987d8751076c81f67ade0cca50cae
Jessica Mauvais <mauvais2@llnl.gov>
2023-04-26 17:07:45 -0700

    added tests for dc_models

---
+++ atomsci/ddm/test/integrative/dc_models/test_reg_config_H1_fit_AttentiveFPModel.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from test_retrain_dc_models import *
+from atomsci.ddm.utils import llnl_utils
+
+# Train and Predict
+# -----
+def test_reg_config_H1_fit_AttentiveFPModel():
+#    if not llnl_utils.is_lc_system():
+#        assert True
+#        return
+        
+    H1_init()
+    train_and_predict('reg_config_H1_fit_AttentiveFPModel.json', prefix='H1') # crashes during run
+
+if __name__ == '__main__':
+    test_reg_config_H1_fit_AttentiveFPModel() #works fine?
---
+++ atomsci/ddm/test/integrative/dc_models/test_reg_config_H1_fit_GCNModel.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from test_retrain_dc_models import *
+from atomsci.ddm.utils import llnl_utils
+
+# -----
+def test_reg_config_H1_fit_GCNModel():
+    H1_init()
+    train_and_predict('reg_config_H1_fit_GCNModel.json', prefix='H1') # crashes during run
+# -----
+
+if __name__ == '__main__':
+    test_reg_config_H1_fit_GCNModel()
